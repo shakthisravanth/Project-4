691c05637a718371cddd9da90bc3052cd9b8cc35
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2025-12-27 12:53:43 +0530

    Update StudentController.java

--- StudentStaticDataToUI/src/main/java/com/controller/StudentController.java
+++ StudentStaticDataToUI/src/main/java/com/controller/StudentController.java
@@ -8,9 +8,14 @@ import com.model.Student;
 @RestController
 public class StudentController {
 
-	@GetMapping("/get")
-	public Student getStudent() {
-		return new Student(1, "Shakthi");
-	}
+	 @GetMapping("/get")
+    public Student getStudent() {
+        return new Student(1, "Shakthi");
+    }
+
+    @PostMapping("/add")
+    public Student setStudent(@RequestBody Student std) {
+        return std;
+    }
 
 }
