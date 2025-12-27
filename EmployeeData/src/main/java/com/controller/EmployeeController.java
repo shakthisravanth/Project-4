c44f4fcc0191df7fa5473515adadde614afdb629
shakthi <shakthisravanth001@gmail.com>
2025-12-27 13:17:24 +0530

    Lab - 3 : EmployeeData

---
+++ EmployeeData/src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.Employee;
+
+
+//mark this class as a REST controller
+
+public class EmployeeController {
+
+	//handle HTTP GET request at /get
+
+	//method to return an Employee object (return type: Employee)
+
+	//handle HTTP POST request at /add
+
+	//method to accept Employee data from request body and return it (return type: Employee)
+
+}
---
+++ EmployeeData/src/main/java/com/model/Employee.java
@@ -0,0 +1,41 @@
+package com.model;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private double sal;
+
+	public Employee() {
+	}
+
+	public Employee(int id, String name, double sal) {
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSal() {
+		return sal;
+	}
+
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+}
