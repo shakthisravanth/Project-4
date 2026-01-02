3814477f21fdbec0fbc291a4cf925a885cea8830
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2026-01-02 12:52:41 +0530

    updated

--- StudentStaticDataGet/src/main/java/com/StudentStaticDataGetApplication.java
+++ StudentStaticDataGet/src/main/java/com/StudentStaticDataGetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentStaticDataToUiApplication {
+public class StudentStaticDataGetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentStaticDataToUiApplication.class, args);
+		SpringApplication.run(StudentStaticDataGetApplication.class, args);
 	}
 
 }
